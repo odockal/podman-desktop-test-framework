cb37bd5a5b3066c364f0ab8c03f6a191768bcf3d
Ondrej Dockal <odockal@redhat.com>
2024-03-05 13:41:05 +0100

    Initial commit
    Signed-off-by: Ondrej Dockal <odockal@redhat.com>

---
+++ src/core.ts
@@ -0,0 +1,9 @@
+// export core modules
+export * from './globalSetup/global-setup';
+export * from './runner/podman-desktop-runner';
+export type { RunnerTestContext } from './testContext/runner-test-context';
+export * from './setupFiles/extended-hooks';
+export * from './setupFiles/setup-registry';
+export * from './utility/cleanup';
+export * from './utility/operations';
+export * from './utility/wait';
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+// export core modules
+export * from './core'
+
+// exports Podman Desktop Page Object Module index file
+export * from './model'
